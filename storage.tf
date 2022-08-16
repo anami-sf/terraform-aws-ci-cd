5ae93c4dedf6b3900e35a4a59a162ae14df7f704
anami <anami.127.0.0.1@gmail.com>
2022-08-16 14:40:49 -0700

    add dynamo table for backend

--- storage.tf
+++ storage.tf
@@ -15,7 +15,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
