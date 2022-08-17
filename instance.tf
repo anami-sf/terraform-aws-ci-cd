0c84891eed9c664c75cf6638d0659b2561c69722
anami <anami.127.0.0.1@gmail.com>
2022-08-16 17:17:52 -0700

    Add EC2 instances

--- instance.tf
+++ instance.tf
@@ -1,5 +1,21 @@
-# resource "aws_instance" "instance_1" {
-#     ami = data.aws_ami.ubuntu
-#     instance_type = t2.data 
-#     security_groups = [ "instance-security-group" ]
-# }
+resource "aws_instance" "instance_1" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instances.name]
+  user_data       = <<-EOF
+            #!/bin/bash
+            echo "Hello, World 1" > index.html
+            python3 -m http.server 8080 &
+            EOF
+}
+
+resource "aws_instance" "instance_2" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instances.name]
+  user_data       = <<-EOF
+            #!/bin/bash
+            echo "Hello, World 2" > index.html
+            python3 -m http.server 8080 &
+            EOF
+}
