5937bac00bdf0f4938da3aa2093cf159cc37d93e
anami <anami.127.0.0.1@gmail.com>
2022-08-17 15:18:40 -0700

    create security group for alb

--- security_groups.tf
+++ security_groups.tf
@@ -11,3 +11,27 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group" "alb" {
+  name = "alb_securtiy_group"
+}
+
+resource "aws_security_group_rule" "allow_alb_http_inbound" {
+  security_group_id = aws_security_group.alb.id
+  type              = "ingress"
+
+  protocol    = "tcp"
+  to_port     = "80"
+  from_port   = "80"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_alb_all_outbound" {
+  security_group_id = aws_security_group.alb.id
+  type              = "egress"
+
+  protocol     = "-1"
+  from_port   = 0
+  to_port     = 0
+  cidr_blocks = ["0.0.0.0/0"]
+}
