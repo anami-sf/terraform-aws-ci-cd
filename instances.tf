34480fc5a4d79a076a8db655230930f611b0f44d
anami <anami.127.0.0.1@gmail.com>
2022-08-17 20:33:37 -0700

    add ssh access to security ec2 group

--- instances.tf
+++ instances.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "instance_1" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  key_name        = "admin-key-pair"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  key_name                    = "admin-key-pair"
   associate_public_ip_address = true
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
+  security_groups             = [aws_security_group.instances.name]
+  user_data                   = <<-EOF
             #!/bin/bash
             echo "Hello, World 1" > index.html
             python3 -m http.server 8080 &
@@ -12,12 +12,12 @@ resource "aws_instance" "instance_1" {
 }
 
 resource "aws_instance" "instance_2" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  key_name        = "admin-key-pair"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  key_name                    = "admin-key-pair"
   associate_public_ip_address = true
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
+  security_groups             = [aws_security_group.instances.name]
+  user_data                   = <<-EOF
             #!/bin/bash
             echo "Hello, World 2" > index.html
             python3 -m http.server 8080 &
--- security_groups.tf
+++ security_groups.tf
@@ -12,6 +12,26 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "server_1_admin_ssh" {
+  security_group_id = aws_security_group.instances.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "SSH access within network"
+}
+
+resource "aws_security_group_rule" "allow_intance_all_outbound" {
+  security_group_id = aws_security_group.instances.id
+  type              = "egress"
+
+  protocol    = "-1"
+  from_port   = 0
+  to_port     = 0
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group" "alb" {
   name = "alb_securtiy_group"
 }
