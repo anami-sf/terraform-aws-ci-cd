64a0137896847f9421d2670c3df5fda8e95d12ef
anami <anami.127.0.0.1@gmail.com>
2022-08-17 21:20:43 -0700

    remove python server

--- instances.tf
+++ instances.tf
@@ -4,11 +4,6 @@ resource "aws_instance" "instance_1" {
   key_name                    = "admin-key-pair"
   associate_public_ip_address = true
   security_groups             = [aws_security_group.instances.name]
-  user_data                   = <<-EOF
-            #!/bin/bash
-            echo "Hello, World 1" > index.html
-            python3 -m http.server 8080 &
-            EOF
 }
 
 resource "aws_instance" "instance_2" {
@@ -17,9 +12,4 @@ resource "aws_instance" "instance_2" {
   key_name                    = "admin-key-pair"
   associate_public_ip_address = true
   security_groups             = [aws_security_group.instances.name]
-  user_data                   = <<-EOF
-            #!/bin/bash
-            echo "Hello, World 2" > index.html
-            python3 -m http.server 8080 &
-            EOF
 }
