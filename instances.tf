fe84bf71359e4d33102cff15801d860a6075f4bd
anami <anami.127.0.0.1@gmail.com>
2022-08-17 19:22:53 -0700

    Make instances public

--- instances.tf
+++ instances.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "instance_1" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
+  key_name        = "admin-key-pair"
+  associate_public_ip_address = true
   security_groups = [aws_security_group.instances.name]
   user_data       = <<-EOF
             #!/bin/bash
@@ -12,6 +14,8 @@ resource "aws_instance" "instance_1" {
 resource "aws_instance" "instance_2" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
+  key_name        = "admin-key-pair"
+  associate_public_ip_address = true
   security_groups = [aws_security_group.instances.name]
   user_data       = <<-EOF
             #!/bin/bash
--- security_groups.tf
+++ security_groups.tf
@@ -6,8 +6,8 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.instances.id
 
-  from_port   = 8080
-  to_port     = 8080
+  from_port   = 80
+  to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
